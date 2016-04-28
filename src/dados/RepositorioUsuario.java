b65b2e8b9d6cd8842ab9c33f3b046f7b27272960
iigorcardoso1 <iigorcardoso@hotmail.com>
2016-04-27 23:49:16 -0300

    buscar e remover usuario

--- src/dados/RepositorioUsuario.java
+++ src/dados/RepositorioUsuario.java
@@ -21,9 +21,11 @@ public class RepositorioUsuario {
 	}
 	
 	public Usuario buscarUsuario(String cpf){
-			int i=0;
+	
+		
+		int i=0;
 			while(this.usuarios[i].getCpf()!=cpf){
-				if(this.usuarios[i].getCpf()==cpf){
+				if(this.usuarios[i].getCpf().equals(cpf)){
 				
 					return this.usuarios[i];
 			}else{
@@ -31,7 +33,18 @@ public class RepositorioUsuario {
 			}
 		}
 			
-		return this.usuarios[i];
+		return null;
+	}
+	
+	public void removerUsuario(Usuario usu){
+		
+		for(int i=0;i<this.usuarios.length;i++){
+			if(this.usuarios[i].equals(usu)){
+				this.usuarios[i]=null;
+				break;
+			}
+		}
+		System.out.println("Usuário Inexistente");
 	}
 	
 	
